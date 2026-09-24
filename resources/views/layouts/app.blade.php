29d363c428b2d674e12705f5244ec11d68961211
hafizkurnia <hafidzkurnia22@gmail.com>
2026-09-24 15:01:08 +0700

    fix: use provided MHK mark as favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,8 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title', 'Portfolio CV')</title>
-    <link rel="icon" type="image/svg+xml" href="{{ asset('favicon.svg') }}?v={{ filemtime(public_path('favicon.svg')) }}">
-    <link rel="shortcut icon" href="{{ asset('favicon.svg') }}?v={{ filemtime(public_path('favicon.svg')) }}">
+    <link rel="icon" type="image/png" href="{{ asset('favicon-mhk.png') }}?v={{ filemtime(public_path('favicon-mhk.png')) }}">
+    <link rel="shortcut icon" type="image/png" href="{{ asset('favicon-mhk.png') }}?v={{ filemtime(public_path('favicon-mhk.png')) }}">
 
     <!-- Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
